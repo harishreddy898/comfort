253e6d7582e230d06353493bb72707ec0334dd42
S Harish reddy <S Harish reddy@Harry865>
2024-04-16 14:41:27 +0530

    meet there

---
+++ uncle/src/uncle/anty/village.java
@@ -0,0 +1,21 @@
+package uncle.anty;
+
+public class village {
+	public static void main (String args[]) {
+			
+	System.out.println("your Names");
+	System.out.println("hsrish");
+	System.out.println("abhi");
+	System.out.println("sritam");
+	System.out.println("rati");
+	System.out.println("gudu");
+	System.out.println();
+	System.out.println("your roll");
+	System.out.println("21");
+	System.out.println("22");
+	System.out.println("23");
+	System.out.println("24");
+	System.out.println("25");
+System.out.println();
+}
+}
